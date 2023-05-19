6d5eb87aad94bfa2db0a7ccd5662875247eabc26
desophos <1887450+desophos@users.noreply.github.com>
2023-05-19 11:27:43 -0500

    Add img alt text

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -4,7 +4,7 @@ import Stack from 'react-bootstrap/Stack';
 export default function Result(props) {
     return (
         <Stack id={props.id} direction="horizontal" gap={1}>
-            <div><img src={props.iconUrl} /></div>
+            <div><img src={props.iconUrl} alt="favicon" /></div>
             <div className="vr" />
             <div>{props.title}</div>
         </Stack>
